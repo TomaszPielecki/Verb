6e0fed1b50fa163d499489ecb6179fc831f4a371
TomaszPielecki <74858305+TomaszPielecki@users.noreply.github.com>
2021-06-12 11:16:29 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!");
+    let x=45;
+    let y=12;
+    let job="developer";
+    println!("age={}",x);
+    println!("y*x={}",y*x);
+    println!("moja praca to {} jest super",job);
+}
